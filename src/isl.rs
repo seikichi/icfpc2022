0c39d0f836d275e7607e3519f817635591bbd829
Seikichi KONDO <seikichi@kmc.gr.jp>
2022-09-02 22:43:55 +0900

    Add ISL types

---
+++ src/isl.rs
@@ -0,0 +1,24 @@
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Block(pub Vec<u32>);
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Point { pub x: u32, pub y: u32}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Color { pub r: f32, pub g: f32, pub b: f32, pub a: f32}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Orientation { Vertical, Horizontal }
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Move {
+    PCut { block: Block, point: Point },
+    LCut { block: Block, orientation: Orientation, linueNumber: i32},
+    Color { block:Block, color: Color },
+    Swap { a: Block, b: Block },
+    Merge { a: Block, b: Block },
+}
+
+// いる？
+pub type Progam = Vec<Move>;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+mod isl;
+
+// Image ? Input type
+
 fn main() {
-    println!("Hello, world!");
+    let a = isl::Block(vec![0]);
+    let b = isl::Block(vec![0]);
+    let m = isl::Move::Swap { a, b };
+    let program = vec![m];
+
+    println!("Hello, Move: {:?}", program);
 }
