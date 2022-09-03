4e875f5e818fe79a035fdb169008add29e8944a6
Yusuke Nojima <nojima@ynojima.com>
2022-09-04 03:51:49 +0900

    fix k-means

--- solver/src/ai/dp.rs
+++ solver/src/ai/dp.rs
@@ -21,9 +21,9 @@ impl HeadAI for DpAI {
     fn solve(&mut self, image: &image::Image, _initial_state: &simulator::State) -> Program {
         // color sampling
         self.image = image.clone();
-        self.sampled_color.push(Color::ONE);
         self.sampled_color =
-            image::k_means_color_sampling(image, self.sample_color_num, 50, &mut self.rng);
+            image::k_means_color_sampling(image, self.sample_color_num - 1, 20, &mut self.rng);
+        self.sampled_color.push(Color::ONE);
 
         // dp
         let d = self.memo.len();
