d4c18044b866fdd52afaf96bfad8fec299549235
Seikichi KONDO <seikichi@kmc.gr.jp>
2022-09-04 01:48:15 +0900

    Update UI to show more info

--- www/pages/runs/[id].tsx
+++ www/pages/runs/[id].tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import type { NextPage, GetServerSideProps } from "next";
 import Link from "next/link";
 
@@ -10,8 +11,6 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import MuiLink from "@mui/material/Link";
-
-import { useEffect } from "react";
 
 import { fetchRun, RunResult } from "../../lib/db";
 import React from "react";
@@ -35,27 +34,58 @@ export const getServerSideProps: GetServerSideProps<Props> = async (
 };
 
 const Page: NextPage<Props> = ({ result }) => {
-  console.log(result);
   return (
-    <>
-      概要:
+    <Stack spacing={2}>
+      <Typography component="h1" variant="h5">
+        {new Date(1000 * result.time).toLocaleString()}
+      </Typography>
+
       <ul>
-        <li>ID: {result.id}</li>
-        <li>Score: {result.score}</li>
-        <li>Date: {new Date(1000 * result.time).toLocaleString()}</li>
-        <li>Args: {result.args}</li>
+        <li>スコア: {result.score}</li>
+        <li>回答数: {result.results.length}</li>
+        <li>
+          引数: <code>{result.args}</code>
+        </li>
       </ul>
-      各タスク:
-      <ul>
-        {result.results.map((r) => (
-          <React.Fragment key={r.problemId}>
-            <li>
-              Problem: {r.problemId}, Score: {r.score}
-            </li>
-          </React.Fragment>
-        ))}
-      </ul>
-    </>
+
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>問題</TableCell>
+              <TableCell>スコア</TableCell>
+              <TableCell>入力</TableCell>
+              <TableCell>出力</TableCell>
+              <TableCell>コミット</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {result.results.map((r) => {
+              const url = `https://github.com/seikichi/icfpc2022/commit/${r.commit}`;
+              const input = `https://cdn.robovinci.xyz/imageframes/${r.problemId}.png`;
+              const output = `https://d30a5x02adw8tj.cloudfront.net/${result.id}/${r.problemId}.png`;
+              return (
+                <TableRow key={r.problemId}>
+                  <TableCell>{r.problemId}</TableCell>
+                  <TableCell>{r.score}</TableCell>
+                  <TableCell>
+                    <img src={input} width="80" alt="input" />
+                  </TableCell>
+                  <TableCell>
+                    <img src={output} width="80" alt="output" />
+                  </TableCell>
+                  <TableCell>
+                    <Link href={url} passHref>
+                      <MuiLink target="_blank">{r.commit}</MuiLink>
+                    </Link>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Stack>
   );
 };
 
