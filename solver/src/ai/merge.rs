e1395634b281e0cf02684ba53a14a17a60d5df04
MORI Shingo <cos@kmc.gr.jp>
2022-09-04 01:38:35 +0900

    add MergeAI

---
+++ solver/src/ai/merge.rs
@@ -0,0 +1,59 @@
+use crate::ai::HeadAI;
+use crate::image;
+use crate::isl::*;
+use crate::simulator;
+use crate::simulator::State;
+
+pub struct MergeAI {
+    state: State,
+}
+
+impl HeadAI for MergeAI {
+    fn solve(&mut self, _image: &image::Image) -> Program {
+        let mut ret = Program(vec![]);
+        while self.active_block_num() > 1 {
+            let mut target = None;
+            for (id1, block1) in self.state.blocks.iter() {
+                if !block1.active {
+                    continue;
+                }
+                for (id2, block2) in self.state.blocks.iter() {
+                    if !block2.active || id1 == id2 {
+                        continue;
+                    }
+                    if let Some(_next_block) = simulator::merge_block(block1, block2) {
+                        target = Some((id1.clone(), id2.clone()));
+                        break;
+                    }
+                }
+                if target.is_some() {
+                    break;
+                }
+            }
+            if let Some((a, b)) = target {
+                ret.0.push(Move::Merge { a, b });
+                simulator::simulate(&mut self.state, ret.0.last().unwrap()).unwrap();
+            } else {
+                panic!("can't find mergable block");
+            }
+        }
+        return ret;
+    }
+}
+
+impl MergeAI {
+    pub fn new(initial_state: &State) -> Self {
+        MergeAI {
+            state: initial_state.clone(),
+        }
+    }
+    fn active_block_num(&self) -> u32 {
+        let mut ret = 0;
+        for block in self.state.blocks.values() {
+            if block.active {
+                ret += 1;
+            }
+        }
+        return ret;
+    }
+}
