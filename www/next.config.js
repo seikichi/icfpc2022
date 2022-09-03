f6d86fdfa2b0f715a2ab7af1de01986191e794ed
Seiichi KONDO <seikichi@kmc.gr.jp>
2022-09-04 04:56:02 +0900

    Update next.config.js

--- www/next.config.js
+++ www/next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+  },
 }
 
 module.exports = nextConfig
