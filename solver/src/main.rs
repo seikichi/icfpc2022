dc40030e8fc3d83da67f4a0eae35093084065e45
Seikichi KONDO <seikichi@kmc.gr.jp>
2022-09-03 18:36:36 +0900

    Update db.rs

--- solver/src/main.rs
+++ solver/src/main.rs
@@ -5,12 +5,12 @@ mod isl;
 mod refine_ai;
 mod simulator;
 
+use anyhow::bail;
 use std::fs;
 use std::path::PathBuf;
-use anyhow::bail;
 use structopt::StructOpt;
 
-use crate::ai::{HeadAI, ChainedAI};
+use crate::ai::{ChainedAI, HeadAI};
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "solver", about = "A solver of ICFPC 2022 problems")]
@@ -27,7 +27,7 @@ struct Opt {
 fn parse_ai_string(ai_str: &str) -> anyhow::Result<(Box<dyn HeadAI>, Vec<Box<dyn ChainedAI>>)> {
     let parts = ai_str.split(',').collect::<Vec<_>>();
     let head_ai: Box<dyn ai::HeadAI> = match parts[0] {
-        "OneColor" => Box::new(ai::OneColorAI{}),
+        "OneColor" => Box::new(ai::OneColorAI {}),
         "Grid" => Box::new(ai::GridAI { rows: 4, cols: 4 }),
         "Cross" => Box::new(ai::CrossAI { size: 3 }),
         x => bail!("'{x}' is not a HeadAI"),
@@ -35,7 +35,7 @@ fn parse_ai_string(ai_str: &str) -> anyhow::Result<(Box<dyn HeadAI>, Vec<Box<dyn
     let mut chained_ais = vec![];
     for name in &parts[1..] {
         let chained_ai: Box<dyn ai::ChainedAI> = match *name {
-            "Refine" => Box::new(refine_ai::RefineAi{}),
+            "Refine" => Box::new(refine_ai::RefineAi {}),
             x => bail!("'{x}' is not a ChainedAI"),
         };
         chained_ais.push(chained_ai);
@@ -43,7 +43,8 @@ fn parse_ai_string(ai_str: &str) -> anyhow::Result<(Box<dyn HeadAI>, Vec<Box<dyn
     Ok((head_ai, chained_ais))
 }
 
-fn main() -> anyhow::Result<()> {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     let opt = Opt::from_args();
 
     let (mut head_ai, chained_ais) = parse_ai_string(&opt.ai)?;
@@ -63,6 +64,15 @@ fn main() -> anyhow::Result<()> {
     output_image.save("result.png")?;
 
     fs::write(opt.output_path, format!("{program}"))?;
+
+    // db::save(
+    //     "482eb33b-b510-4e06-bec9-9222159deaee",
+    //     1,
+    //     &program,
+    //     score,
+    //     "result.png",
+    // )
+    // .await?;
 
     Ok(())
 }
