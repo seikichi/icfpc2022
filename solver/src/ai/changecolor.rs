6dbbc9851341b8cbfce7dd0d39a82ff179cfb5cf
Kohei Nagira <qwerty@kmc.gr.jp>
2022-09-04 13:54:45 +0900

    rename local variables

--- solver/src/ai/changecolor.rs
+++ solver/src/ai/changecolor.rs
@@ -11,11 +11,11 @@ impl HeadAI for ChangeColorAI {
     fn solve(&mut self, image: &image::Image, initial_state: &simulator::State) -> isl::Program {
         let mut programs = vec![];
         for (block_id, block) in initial_state.blocks.clone().iter() {
-            let color_ave = image.average(block.p, block.size);
+            let color_average = image.average(block.p, block.size);
             let color_majority = image.majority(block.p, block.size);
-            let next_move_ave = isl::Move::Color {
+            let next_move_average = isl::Move::Color {
                 block_id: block_id.clone(),
-                color: color_ave,
+                color: color_average,
             };
             let next_move_majority = isl::Move::Color {
                 block_id: block_id.clone(),
@@ -27,22 +27,40 @@ impl HeadAI for ChangeColorAI {
                 block.color,
                 image,
             );
-            let sim_ave =
-                simulator::calc_partial_one_color_similarity(block.p, block.size, color_ave, image);
-            let sim_majority =
-                simulator::calc_partial_one_color_similarity(block.p, block.size, color_majority, image);
-            let move_cost_ave =
-                simulator::move_cost(initial_state, &next_move_ave, image.width(), image.height())
-                    .unwrap();
-            let move_cost_majority =
-                simulator::move_cost(initial_state, &next_move_majority, image.width(), image.height())
-                    .unwrap();
-            if sim_ave + move_cost_ave >= sim_before && sim_majority + move_cost_majority >= sim_before {
+            let sim_average = simulator::calc_partial_one_color_similarity(
+                block.p,
+                block.size,
+                color_average,
+                image,
+            );
+            let sim_majority = simulator::calc_partial_one_color_similarity(
+                block.p,
+                block.size,
+                color_majority,
+                image,
+            );
+            let move_cost_average = simulator::move_cost(
+                initial_state,
+                &next_move_average,
+                image.width(),
+                image.height(),
+            )
+            .unwrap();
+            let move_cost_majority = simulator::move_cost(
+                initial_state,
+                &next_move_majority,
+                image.width(),
+                image.height(),
+            )
+            .unwrap();
+            if sim_average + move_cost_average >= sim_before
+                && sim_majority + move_cost_majority >= sim_before
+            {
                 continue;
-            } else if sim_ave + move_cost_ave >= sim_majority + move_cost_majority {
+            } else if sim_average + move_cost_average >= sim_majority + move_cost_majority {
                 programs.push(next_move_majority)
             } else {
-                programs.push(next_move_ave)
+                programs.push(next_move_average)
             }
         }
         isl::Program(programs)
